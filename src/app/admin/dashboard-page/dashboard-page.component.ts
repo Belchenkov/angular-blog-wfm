46e3cf6ad5f43bf9cdc6dfb024ceaaba8c216cf1
Aleksey Belchenkov <u608110@gmail.com>
2020-03-24 16:21:14 +0300

    Pipe for posts

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ import {Post} from '../../shared/interfaces';
 export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   pSub: Subscription;
+  searchStr: '';
 
   constructor(private postsService: PostsService) { }
 
